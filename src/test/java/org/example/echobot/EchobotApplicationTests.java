88e17f7ebf8c53b2e63c99f26c69732854cf0518
Alexandr <alexandr6732@mail.ru>
2024-11-17 10:00:20 +0300

    initialization

---
+++ src/test/java/org/example/echobot/EchobotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.echobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
